fe29d02883dd934883586b6df5f32a932216bfb0
Matt Pindell <pindell.m@gmail.com>
2016-03-24 13:14:11 -0600

    added full test coverage for models

--- test/models/robot_world_test.rb
+++ test/models/robot_world_test.rb
@@ -37,7 +37,7 @@ class RobotWorldTest < Minitest::Test
     assert_equal "Steven Urkel", robots.last.name
   end
 
-  def test_it_can_return_find_a_robot
+  def test_it_can_find_a_robot
     robot_world.create({
       :name       => "Charles Bronson",
       :avatar     => "a description",
@@ -56,4 +56,61 @@ class RobotWorldTest < Minitest::Test
     assert_equal "a description", robot.avatar
     assert_equal 2, robot.id
   end
+
+  def test_it_can_update_a_robot
+    robot_world.create({
+      :name       => "Charles Bronson",
+      :avatar     => "a description",
+      :birth_date => "12/12/2012"
+    })
+
+    robot = robot_world.find(1)
+
+    assert_equal "Charles Bronson", robot.name
+    assert_equal "a description", robot.avatar
+    assert_equal "12/12/2012", robot.birth_date
+    assert_equal 1, robot.id
+
+    updated = {
+      name: "Fancy Pants",
+      avatar: "pants2fancy",
+      birth_date: "12/12/2011"
+    }
+
+    robot_world.update(updated, 1)
+    robot = robot_world.find(1)
+
+    assert_equal "Fancy Pants", robot.name
+    assert_equal "pants2fancy", robot.avatar
+    assert_equal "12/12/2011", robot.birth_date
+    assert_equal 1, robot.id
+  end
+
+  def test_it_can_delete_a_robot
+    robot_world.create({
+      :name       => "Charles Bronson",
+      :avatar     => "a description",
+      :birth_date => "12/12/2012"
+    })
+
+    robot_world.create({
+      :name       => "Steven Urkel",
+      :avatar     => "a description",
+      :birth_date => "12/12/2012"
+    })
+
+    robots = robot_world.all
+    assert_kind_of Array, robots
+    assert_equal 2, robots.count
+    assert_equal "Charles Bronson", robots.first.name
+    assert_equal "Steven Urkel", robots.last.name
+
+    robot_world.destroy(1)
+    robots = robot_world.all
+
+    assert_kind_of Array, robots
+    assert_equal 1, robots.count
+    assert_equal "Steven Urkel", robots.first.name
+  end
+
 end
